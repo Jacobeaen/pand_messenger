0182158c3792f5774632c7459ef2e3457153bb9f
Jacobeaen-Laptop <alexander.pushilov@gmail.com>
2025-07-31 01:51:33 +0300

    client send data to server.  Errors and makefile fixes

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ CXXFLAGS = -Wall -I include
 SRC = src/client.cpp src/errors.cpp
 
 BIN_DIR = build
-BIN = $(BIN_DIR)/app.out
+BIN = $(BIN_DIR)/app
 
+all: $(BIN)
 $(BIN_DIR):
 	mkdir -p $(BIN_DIR)
 
--- src/errors.cpp
+++ src/errors.cpp
@@ -2,28 +2,28 @@
 #include <stdio.h>
 
 typedef enum {
-    netfunc_code_check,
-    getfunc_code_check,
-    socket_code_check,
+    netfunc_code,
+    getfunc_code,
+    socket_code,
 } err_type;
 
 
 // inet_pton и др. возвращает 1 при успехе, ф-ции сокетов -1 при ошибке, getaddrinfo 0 при успехе
 void print_error(int code, const char *message, err_type type) {
     switch (type) {
-        case netfunc_code_check:
+        case netfunc_code:
             if (code != 1) {
                 perror(message);
                 exit(EXIT_FAILURE);
             }   
             break;
-        case getfunc_code_check:
+        case getfunc_code:
             if (code != 0) {
                 perror(message);
                 exit(EXIT_FAILURE);
             }
             break;
-        case socket_code_check:
+        case socket_code:
             if (code == -1) {
                 perror(message);
                 exit(EXIT_FAILURE);
